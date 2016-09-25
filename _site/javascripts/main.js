0b04827e197f152744513e002770e091e5c74acd
ibeauvais <ivan.beauvais@gmail.com>
2016-09-25 16:52:30 +0200

    step 3

--- _site/javascripts/main.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function() {
-  jQuery(".solution").wrap('<div class = "spoiler"></div>')
-  jQuery(".solution").before('<button  type="button" class = "teaser">Afficher la solution</button>')
-  jQuery(".solution").hide();
-  jQuery(document).on('click', '.teaser', function() {
-    jQuery(this).next(".solution").slideToggle(500);
-  });
-});
